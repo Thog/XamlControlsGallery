e5d4108f844126b000d3646116d3c0d29cd39a0e
Mary <me@thog.eu>
2021-03-02 23:21:13 +0100

    Seems to be ProgressBar

--- XamlControlsGallery/ViewModels/MainWindowViewModel.cs
+++ XamlControlsGallery/ViewModels/MainWindowViewModel.cs
@@ -6,33 +6,5 @@ namespace XamlControlsGallery.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private bool _isMenuItemChecked;
-
-        public MainWindowViewModel()
-        {
-            ToggleMenuItemCheckedCommand = ReactiveCommand.Create(() =>
-            {
-                IsMenuItemChecked = !IsMenuItemChecked;
-            });
-        }
-
-        public bool IsMenuItemChecked
-        {
-            get { return _isMenuItemChecked; }
-            set { this.RaiseAndSetIfChanged(ref _isMenuItemChecked, value); }
-        }
-
-        public ReactiveCommand<Unit, Unit> ToggleMenuItemCheckedCommand { get; }
-
-        public string Greeting => "Welcome to Avalonia!";
-
-        public List<string> SearchItems { get; } = new List<string>
-        {
-            "TextBlock",
-            "CheckBox",
-            "ComboBox",
-            "TextBox",
-            "Calendar"
-        };
     }
 }
